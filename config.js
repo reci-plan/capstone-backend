32707603d2da4b1ae4d6f3e2e5733bb84825c80d
mpadil18 <63021608+mpadil18@users.noreply.github.com>
2021-08-03 15:26:29 -0700

    Add backend files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3001;
+const API_KEY = process.env.API_KEY;
+const API_KEY2 = process.env.API_KEY2;
+const SECRET_KEY = process.env.SECRET_KEY || "recipath";
+const BASE_RECIPES_URL = "https://api.spoonacular.com/recipes";
+const BCRYPT_WORK_FACTOR = 10;
+
+function getDatabaseUri() {
+    const dbUser = process.env.DATABASE_USER || "postgres";
+    const dbPass = process.env.DATABASE_PASS
+        ? encodeURI(process.env.DATABASE_PASS)
+        : "postgres";
+    const dbHost = process.env.DATABASE_HOST || "localhost";
+    const dbPort = process.env.DATABASE_PORT || 5432;
+    const dbName = process.env.DATABASE_NAME || "recipath";
+
+    return (
+        process.env.DATABASE_URL ||
+        `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+    );
+}
+
+module.exports = {
+    PORT,
+    API_KEY,
+    API_KEY2,
+    BASE_RECIPES_URL,
+    SECRET_KEY,
+    getDatabaseUri,
+    BCRYPT_WORK_FACTOR,
+};
